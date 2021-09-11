ff0a3a5d5b16ff8be3b79de43e8a4b744ac624ab
Jabari Tucker <jabari_tucker@hotmail.com>
2021-09-11 16:01:22 -0600

    Added earthquake data

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -1,9 +1,6 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with center and zoom level.
-// let map = L.map('mapid').setView([30, 30], 2);
-
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -21,37 +18,34 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
     "Streets": streets,
-    "Satellite Streets": satelliteStreets
+    "Satellite": satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-    center: [43.7, -79.3],
-    zoom: 11,
+    center: [39.5, -98.5],
+    zoom: 3,
     layers: [streets]
 });
 
 // Pass our map layers into our layers control and add the layers control to the map.
 L.control.layers(baseMaps).addTo(map);
 
-// Accessing the Toronto neighborhoods GeoJSON URL.
-let torontoHoods = "https://raw.githubusercontent.com/tzdwq8/Mapping_Earthquakes/main/torontoNeighborhoods.json";
-
 // Create a style for the lines.
-let myStyle = {
-    color: "blue",
-    weight: 1,
-    fillColor: "yellow"
-}
+// let myStyle = {
+    // color: "blue",
+    // weight: 1,
+    // fillColor: "yellow"
+// }
 
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
     console.log(data);
 // Creating a GeoJSON layer with the retrieved data.
-L.geoJson(data, {
-    style: myStyle,
-    onEachFeature: function(feature, layer) {
-        layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
-    }
-}).addTo(map);
+L.geoJson(data).addTo(map);
+    // style: myStyle,
+    // onEachFeature: function(feature, layer) {
+        // layer.bindPopup("<h3>" + "Neighborhood: " + feature.properties.AREA_NAME + "</h3>");
+    // }
+// }).addTo(map);
 });
